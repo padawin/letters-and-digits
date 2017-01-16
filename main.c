91a5d7d73ad37174ef57d20e0a7da45cd591ee6a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-16 16:07:25 +0000

    mode super easy implemented

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -9,7 +10,9 @@ void startHard();
 void startSuperHard();
 
 char readChar();
+void readString(char* str);
 void init();
+char generateLetter(char min, char max);
 
 char readChar() {
 	char ret;
@@ -19,6 +22,14 @@ char readChar() {
 	ret = in[0];
 	free(in);
 	return ret;
+}
+
+void readString(char str[3]) {
+	size_t sizeChoice = 3;
+	char* in = malloc(sizeof(char) * sizeChoice);
+	getline(&in, &sizeChoice, stdin);
+	strncpy(str, in, 3);
+	free(in);
 }
 
 int main() {
@@ -77,8 +88,35 @@ void init() {
 	srand((unsigned) time(&t));
 }
 
+char generateLetter(char min, char max) {
+	return min + rand() % (max + 1 - min);
+}
+
 void startSuperEasy() {
 	init();
+	char letter = generateLetter('A', 'Z');
+	char determineLetter = rand() % 2;
+	if (determineLetter) {
+		printf("Which letter is %d? ", letter - 'A' + 1);
+		char choice = readChar();
+		if (choice == letter) {
+			printf("Congratulations!\n");
+		}
+		else {
+			printf("No! %d is %c.\n", letter - 'A' + 1, letter);
+		}
+	}
+	else {
+		printf("Which value is %c? ", letter);
+		char choice[3];
+		readString(choice);
+		if ('A' - 1 + atoi(choice) == letter) {
+			printf("Congratulations!\n");
+		}
+		else {
+			printf("No! %c is %d.\n", letter, letter - 'A' + 1);
+		}
+	}
 }
 
 void startEasy() {
